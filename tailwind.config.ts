db7b3f7ddbd812f3824aaf8eef8bb01b12eb63f4
tami <tamiortami@gmail.com>
2025-04-05 11:25:41 +0200

    feat: react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",  
-  ],
+    /** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-       inter: ['Inter', 'sans-serif'],  
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
+
   plugins: [],
-}
+};
